3c5f370dbbcee51d014b1e666995d24e067c0b90
wpghks <wpghks56@naver.com>
2023-06-27 13:58:46 +0900

    [init] 초기 설정

--- lib/prev_next.dart
+++ lib/prev_next.dart
@@ -1,7 +1,3 @@
 library prev_next;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'widget/prev_next.dart';
---
+++ lib/widget/prev_next.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrevNext extends StatefulWidget {
+  const PrevNext({Key? key}) : super(key: key);
+
+  @override
+  _PrevNextState createState() => _PrevNextState();
+}
+
+class _PrevNextState extends State<PrevNext> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
